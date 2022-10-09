80974e83bb76f3f42db0ec823a9de6bf9776fa6a
Ayush <114813540+Ayushg1@users.noreply.github.com>
2022-10-09 18:17:40 +0530

    Update stream_app.py
    create one dialog box to add the fruit name they want

--- stream_app.py
+++ stream_app.py
@@ -46,3 +46,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contain:")
 streamlit.dataframe(my_data_rows)# it will show the sf fetch data
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thanks for adding ', add_my_fruit)
