4c8b235f7a264a650274756c462204a8f689cc9a
Ayush <114813540+Ayushg1@users.noreply.github.com>
2022-10-09 18:48:32 +0530

    Update stream_app.py
    create one function and call inside try and except block

--- stream_app.py
+++ stream_app.py
@@ -29,6 +29,11 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 #streamlit.write('The user entered ', fruit_choice)
 #import requests
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
@@ -36,10 +41,12 @@ try:
 #streamlit.text(fruityvice_response.json())#just writes the data to the screen
 # take the json response and normalize it
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    back_from_function = get_fruityvice_data(fruit_choice)
     # output it to the screen as table
-    streamlit.dataframe(fruityvice_normalized)
+    #streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
   
